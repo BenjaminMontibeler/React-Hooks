74265858a41018bf31f9d9747ac110bb9d583989
Benjamin Montibeler <benjaminmontibeler@gmail.com>
2023-01-30 03:24:47 +0100

    First 5 lessons completed

---
+++ state-hooks/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+import HookCounterFour from './components/HookCounterFour';
+import HookCounterThree from './components/HookCounterThree';
+import HookCounterTwo from './components/HookCounterTwo';
+
+function App() {
+  return (
+    <div className="App">
+      <HookCounterFour />
+      {/* <HookCounterThree /> */}
+      {/* <HookCounterTwo /> */}
+      {/* <ClassCounter />
+      <HookCounter /> */}
+    </div>
+  );
+}
+
+export default App;
